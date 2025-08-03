fc77915fd2ea74290a9f40a75b27b2ae3ec77b4c
Mohammed Sinan K <mohammedsinan.k.33@gmail.com>
2025-08-03 22:48:59 +0530

    fix: Update import paths after moving LandingPage component

--- tivento/src/components/Pages/LandingPage.tsx
+++ tivento/src/components/Pages/LandingPage.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Header from '@/components/LandingPage/Header';
-import Hero from '../components/LandingPage/Hero';
-import HowItWorks from '../components/LandingPage/HowItWorks';
-import UpcomingEvents from '../components/LandingPage/UpcomingEvents';
-import TopCategories from '../components/LandingPage/TopCategories';
-import CreateGroupCTA from '../components/LandingPage/CreateGroupCTA';
-import Footer from '../components/LandingPage/Footer';
+import Hero from '@/components/LandingPage/Hero';
+import HowItWorks from '@/components/LandingPage/HowItWorks';
+import UpcomingEvents from '@/components/LandingPage/UpcomingEvents';
+import TopCategories from '@/components/LandingPage/TopCategories';
+import CreateGroupCTA from '@/components/LandingPage/CreateGroupCTA';
+import Footer from '@/components/LandingPage/Footer';
 
 const LandingPage = () => {
   return (
